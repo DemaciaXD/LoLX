d9197d2a51d65d8a82ab3d812006f50e96d35da7
rocwangv <rocwangi@sina.cn>
2017-08-17 15:19:37 +0800

    reset project

---
+++ 03_Development/LOLX/LOLX/Classes/Src/BaseClass/WGNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  WGNavigationController.swift
+//  LOLX
+//
+//  Created by V on 2017/8/14.
+//  Copyright © 2017年 com.fengxian. All rights reserved.
+//
+
+import UIKit
+
+class WGNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ 03_Development/LOLX/LOLX/Classes/Utils/Network/WGLolAPI.swift
@@ -0,0 +1,112 @@
+//
+//  WGLolAPI.swift
+//  LOLX
+//
+//  Created by V on 2017/8/14.
+//  Copyright © 2017年 com.fengxian. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+// MARK: - Provider setup
+
+private func JSONResponseDataFormatter(_ data: Data) -> Data {
+    do {
+        let dataAsJSON = try JSONSerialization.jsonObject(with: data)
+        let prettyData =  try JSONSerialization.data(withJSONObject: dataAsJSON, options: .prettyPrinted)
+        return prettyData
+    } catch {
+        return data // fallback to original data if it can't be serialized.
+    }
+}
+
+let LolProvider = MoyaProvider<LOLX>(plugins: [NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter)])
+
+private extension String {
+    var urlEscaped: String {
+        return self.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    }
+}
+
+public enum LOLX {
+    case myHeros(String, String);
+}
+
+extension LOLX: TargetType {
+    public var baseURL: URL { return URL(string: "http://115.28.43.55/lol/index.php/MyHeros/execute/target/")! }
+    public var path: String {
+        switch self {
+        case .myHeros(let userName, let serverName):
+            return "\(userName.urlEscaped)/serverName/\(serverName.urlEscaped)";
+        }
+    }
+    public var method: Moya.Method {
+        return .get
+    }
+    public var parameters: [String: Any]? {
+        switch self {
+        case .myHeros(_, _):
+            return ["userName": "RocWang丶",
+                    "serverName": "网通三"];
+        }
+    }
+    public var parameterEncoding: ParameterEncoding {
+        return URLEncoding.default
+    }
+    public var task: Task {
+        return .request
+    }
+    public var validate: Bool {
+        switch self {
+        case .myHeros(_, _):
+            return true
+        }
+    }
+    public var sampleData: Data {
+        switch self {
+        case .myHeros(let userName, let serverName):
+            return "{\"userName\": \"\(userName)\", \"serverName\": \"\(serverName)\"".data(using: String.Encoding.utf8)!
+        }
+    }
+    public var headers: [String: String]? {
+        return nil
+    }
+}
+
+public func url(_ route: TargetType) -> String {
+    return route.baseURL.appendingPathComponent(route.path).absoluteString
+}
+
+//MARK: - Response Handlers
+
+extension Moya.Response {
+    func mapNSArray() throws -> NSArray {
+        let any = try self.mapJSON()
+        guard let array = any as? NSArray else {
+            throw MoyaError.jsonMapping(self)
+        }
+        return array
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
